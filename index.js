81b66c0be074fd63ae97f15e658c148b5ac32d24
markoz <markdrew53@gmail.com>
2020-06-09 18:32:14 -0500

    Fix typo

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-const { config } = require('./config');
-
-module.exports = config
-
+module.exports = {
+  parser: 'babel-eslint',
+    parserOptions: {
+        ecmaVersion: 11,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    env: {
+        node: true,
+        browser: true,
+        es6: true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended'
+    ],
+    plugins: [
+        'react-hooks'
+    ],
+    rules: {
+       'react/react-in-jsx-scope': 'off',
+       'react-hooks/rules-of-hooks': 'error',
+       'react-hooks/exhaustive-deps': 'warn'
+    }
+};
